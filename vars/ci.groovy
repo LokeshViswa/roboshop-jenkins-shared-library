05667223c1211d33f0cbbec55fd04ef6afb5b647
LokeshV <lokeshviswa428@gmail.com>
2023-02-14 14:42:55 +0530

    Roboshop Implementation in Jenkins

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -2,6 +2,11 @@ def call() {
     if(!env.SONAR_EXTRA_OPTS) {
         env.SONAR_EXTRA_OPTS = " "
     }
+    if(!env.TAG_NAME) {
+        env.PUSH_CODE = "false"
+    } else {
+        env.PUSH_CODE = "true"
+    }
     try {
         node('workstation') {
 
@@ -27,10 +32,13 @@ def call() {
                     sh "echo Sonar Scan"
                 }
             }
+            if(env.PUSH_CODE == "true") {
+                stage('Upload Code to Centralized Place') {
+                    echo 'Upload'
+                }
 
-            stage('Upload Code to Centralized Place') {
-                echo 'Upload'
-            }
+                }
+
 
 
 
